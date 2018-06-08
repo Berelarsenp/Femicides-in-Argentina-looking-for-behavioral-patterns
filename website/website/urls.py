551f5965bf0816e4d436c619b3f0cd1e18f98bea
Jordan Winfery <jwinf843@gmail.com>
2018-06-08 01:53:43 +0000

    Deployed successfully to Heroku and added CSS and JS stylings.

--- website/website/urls.py
+++ website/website/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from femi import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #Always leave the index url at the bottom
+    path('', views.index),
 ]
